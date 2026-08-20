2f521cef14ca1d40b60c116164f7e014bc798096
jke81157-pixel <jke81157@gmail.com>
2026-08-20 22:27:39 +0100

    Add files via upload

---
+++ GreetingScreenshotTest.kt
@@ -0,0 +1,44 @@
+package com.example
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.unit.dp
+import com.example.ui.components.AdSlot
+import com.example.ui.components.HeroSection
+import com.example.ui.theme.NanoVisionTheme
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.captureRoboImage
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(RobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(qualifiers = RobolectricDeviceQualifiers.Pixel8, sdk = [36])
+class GreetingScreenshotTest {
+
+    @get:Rule val composeTestRule = createComposeRule()
+
+    @Test
+    fun studio_screenshot() {
+        composeTestRule.setContent {
+            NanoVisionTheme(darkTheme = true) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    HeroSection(
+                        onNavigateToImage = {},
+                        onNavigateToVideo = {}
+                    )
+                    AdSlot(adSlotId = "test_slot")
+                }
+            }
+        }
+
+        composeTestRule.onRoot().captureRoboImage(filePath = "src/test/screenshots/studio.png")
+    }
+}
